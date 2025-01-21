dd4658db338a73d65fcb272b4c26b9235d561fa7
valentinefleith <valentine.fleith@gmail.com>
2025-01-21 15:00:52 +0100

    use click for CLI

--- wordz.py
+++ wordz.py
@@ -1,7 +1,7 @@
-import sys
 from collections import Counter
 from typing import Iterable
 import regex
+import click
 
 TOKEN_PATTERN = regex.compile(r"(?u)\b\w\w+\b")
 
@@ -13,24 +13,19 @@ def compte_mots(texte: Iterable[str]) -> Counter[str]:
     return res
 
 
-def main():
-    try:
-        inpt = sys.argv[1]
-    except IndexError:
-        print("Invalid command. Usage: wordz INPT [WORD].")
-        return
+@click.command()
+@click.argument("inpt")
+@click.argument("word", required=False)
+def main(inpt: str, word: str | None):
 
     with open(inpt) as in_stream:
         count = compte_mots(in_stream)
 
-    if len(sys.argv) == 3:
-        w = sys.argv[2]
-        print(f"{w}: {count[w]}")
-    elif len(sys.argv) == 2:
+    if word is not None:
+        print(f"{word}: {count[word]}")
+    else:
         for w, count in count.most_common(16):
             print(f"{w}: {count}")
-    else:
-        print("Invalid command. Usage: wordz INPT [WORD].")
 
 
 if __name__ == "__main__":
