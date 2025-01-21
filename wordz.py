f68a3216fdcf41f8abbe8ab799e97d9b014c9650
valentinefleith <valentine.fleith@gmail.com>
2025-01-21 14:43:52 +0100

    :art:

--- wordz.py
+++ wordz.py
@@ -1,11 +1,15 @@
 import sys
 from collections import Counter
 from typing import Iterable
+import regex
+
+TOKEN_PATTERN = regex.compile(r"(?u)\b\w\w+\b")
+
 
 def compte_mots(texte: Iterable[str]) -> Counter[str]:
     res = Counter()
     for line in texte:
-        res.update(line.split())
+        res.update(TOKEN_PATTERN.findall(line))
     return res
 
 
